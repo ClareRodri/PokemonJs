18b59985e12bdc7f668202a40357c4ce15e2e449
Clarena <clarero1611@gmail.com>
2014-08-15 19:06:32 -0500

    add-background
    Nuevo diseño e imagenes.

--- js/poke.js
+++ js/poke.js
@@ -1,7 +1,7 @@
-var pokemon=[ 	["Pikachu","Charmander","Toggepi"],
-				["70","65","10"], // Ataque
-				["89","92","100"], // Vida
-				["Electrico","Fuego","--"] // Resistencia
+var pokemon=[ 	["Pikachu","Charmander","Toggepi","Meowth"],
+				["70","65","10","60"], // Ataque
+				["89","92","100","40"], // Vida
+				["Electrico","Fuego","Normal","Normal"] // Tipo
 			];
 
 function pokeDatos(e){	
@@ -10,13 +10,19 @@ function pokeDatos(e){
 	this.ataque = pokemon[1][this.elige];
 	this.vida = pokemon[2][this.elige];
 	this.tipo = pokemon[3][this.elige];
-	document.write("<img src='img/"+this.nombre+".png' >");
-	console.log("Poke = "+this.nombre+"\n"+"Ataque = "+this.ataque+"\n"+"Vida = "+this.vida+"\n"+"Tipo = "+this.tipo);	
+	//document.write("<img src='img/"+this.nombre+".png' class='imgUp1' >");
+	//document.write("<br>Poke = "+this.nombre+"<br>"+"Ataque = "+this.ataque+"<br>"+"Vida = "+this.vida+"<br>"+"Tipo = "+this.tipo);	
 	
 }
 
 function inicio(){
-	var elige = Math.floor((Math.random() * 3)); 
-	pokeDatos(elige);
-
+	var elige = Math.floor((Math.random() * 4)); 
+	var poke= new pokeDatos(elige);
+	nombre.innerText=poke.nombre;
+//	vidaPokemon.innerText=poke.vida;
+//	ataquePokemon.innerText=poke.ataque;
+//	tipoPokemon.innerText=poke.tipo;
+	document.getElementById('img').innerHTML="<img src='img/"+poke.nombre+".png'>";
+	document.getElementById("card").style.background = "url('img/back-"+poke.tipo+".png')";
+	console.log("NAME= "+name.innerText);
 }
