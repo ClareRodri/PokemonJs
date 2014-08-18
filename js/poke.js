a591556fff8f97772730a995d2f870b19924dc00
Clarena <clarero1611@gmail.com>
2014-08-17 22:31:34 -0500

    add-tipo-logo
    Se añade loguitos de tipo y la fuente para el nombre del pokemón.

--- js/poke.js
+++ js/poke.js
@@ -1,28 +1,36 @@
-var pokemon=[ 	["Pikachu","Charmander","Toggepi","Meowth"],
-				["70","65","10","60"], // Ataque
-				["89","92","100","40"], // Vida
-				["Electrico","Fuego","Normal","Normal"] // Tipo
+var pokemon=[ 	["Pikachu","Charmander","Toggepi","Meowth","Squirtle","Bulbasaur"],
+				["70","65","10","60","50","45"], // Ataque
+				["Todo Pokémon que le golpee con un ataque de contacto tiene un 30% de probabilidades de resultar paralizado."+
+				"Hasta Quinta Generación, puede paralizar a Pokémon de tipo Tierra.",
+				 "Si el Pokémon con esta habilidad tiene igual o menos del 33% de su PS máximo, el poder de sus ataques de tipo Fuego aumenta en un 50%.",
+				 "Aumenta el Ataque del usuario en un 50%, pero reduce la Precisión de sus ataques físicos en un 20%. Esta habilidad no afecta a los ataques especiales ni a los movimientos de estado del Pokémon.",
+				 "Tiene un 10% de probabilidades de recoger un objeto del suelo tras cada batalla. No debe llevar ningún objeto equipado para que funcione.",
+				 "Si el Pokémon con esta habilidad tiene igual o menos del 33% de su PS máximo, el poder de sus ataques de tipo Agua aumenta en un 50%.",
+				 "La Velocidad del Pokémon aumenta un 100% (se duplica) en Día Soleado. No se incluye el turno en el que se activa el sol."],
+				["89","92","100","40","79","90"], // Vida actual
+				["90","100","100","60","100","97"], // Vida
+				["Electrico","Fuego","Normal","Normal","Agua","Hierba"] // Tipo
 			];
 
 function pokeDatos(e){	
 	this.elige=e;
 	this.nombre = pokemon[0][this.elige];
 	this.ataque = pokemon[1][this.elige];
-	this.vida = pokemon[2][this.elige];
-	this.tipo = pokemon[3][this.elige];
-	//document.write("<img src='img/"+this.nombre+".png' class='imgUp1' >");
-	//document.write("<br>Poke = "+this.nombre+"<br>"+"Ataque = "+this.ataque+"<br>"+"Vida = "+this.vida+"<br>"+"Tipo = "+this.tipo);	
-	
+	this.poder = pokemon[2][this.elige];
+	this.vidaA = pokemon[3][this.elige];
+	this.vida = pokemon[4][this.elige];
+	this.tipo = pokemon[5][this.elige];		
 }
 
 function inicio(){
-	var elige = Math.floor((Math.random() * 4)); 
+	var elige = Math.floor((Math.random() * 6)); 
 	var poke= new pokeDatos(elige);
 	nombre.innerText=poke.nombre;
-//	vidaPokemon.innerText=poke.vida;
-//	ataquePokemon.innerText=poke.ataque;
-//	tipoPokemon.innerText=poke.tipo;
-	document.getElementById('img').innerHTML="<img src='img/"+poke.nombre+".png'>";
+
+	document.getElementById('logo').innerHTML="<img src='img/logo_"+poke.tipo+".png' style='border-style: none;'>";
+	document.getElementById('img').innerHTML="<img src='img/"+poke.nombre+".png' class='imgs'>";
 	document.getElementById("card").style.background = "url('img/back-"+poke.tipo+".png')";
-	console.log("NAME= "+name.innerText);
+	document.getElementById('poder').innerHTML="<p>"+poke.poder+"</p>";
+	document.getElementById('vida').innerHTML="<b> Vida: "+poke.vidaA+"/"+poke.vida+"</b>";
+	document.getElementById('ataque').innerHTML="<b> Ataque: "+poke.ataque+"</b>";
 }
